bb8146a884e345dcb8c06c53eccaa9084e86ff36
laurayess <56653712+laurayess@users.noreply.github.com>
2023-03-04 23:24:01 -0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,7 +1,1 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js", {
-      scope: "/MIGRACION-AEROPUERTO/#/",
-    });
-  });
-}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
