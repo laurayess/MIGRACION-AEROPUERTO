6d6ddd444f572f59dcd74e5c0039152a76f0b400
laurayess <56653712+laurayess@users.noreply.github.com>
2023-03-04 23:49:44 -0600

    corrigiendo pwa

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+    <HashRouter>
+      <Provider store={store}>
+        {/* <BrowserRouter> */}
         <App />
-      </BrowserRouter>
-    </Provider>
+        {/* </BrowserRouter> */}
+      </Provider>
+    </HashRouter>
   </React.StrictMode>
 );
